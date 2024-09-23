0427d52340534cea9fb2dbe0b8bf96e0a7db4c00
taetae <dlaxodus1212@naver.com>
2024-09-24 01:34:33 +0900

    동작 파라미터화

---
+++ src/chapter_02/ApplePredicate.java
@@ -0,0 +1,5 @@
+package chapter_02;
+
+public interface ApplePredicate {
+    boolean test(Apple apple);
+}
--- src/chapter_02/FilterApple.java
+++ src/chapter_02/FilterApple.java
@@ -2,22 +2,23 @@ package chapter_02;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import static chapter_02.FilterApple.Color.GREEN;
+import static chapter_02.FilterApple.Color.RED;
 
 public class FilterApple {
 
-    enum Color {RED, GREEN}
-
-    public static List<FilterApple> filterGreenApples(List<FilterApple> inventory, Color color) {
-        List<FilterApple> result = new ArrayList<>();
-
-        for (FilterApple apple : inventory) {
-            if(apple.getColor().equals(color)){
-                result.add(apple);
-            }
+    public class AppleHeavyWeightPredicate implements ApplePredicate {
+        public boolean test(Apple apple) {
+            return apple.getWeight() > 150;
         }
-        return result;
+    }
+
+    public class AppleGreenColorPredicate implements ApplePredicate {
+        public boolean test(Apple apple) {
+            return GREEN.equals(apple.getColor());
+        }
     }
 
 }
