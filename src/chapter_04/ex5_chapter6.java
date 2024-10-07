5f6d6c66bd3adc9e1b84cd6fb58c828e3c849b8e
taetae <dlaxodus1212@naver.com>
2024-10-07 22:42:41 +0900

    요약 연산에 대한 메서드들

--- src/chapter_04/ex5_chapter6.java
+++ src/chapter_04/ex5_chapter6.java
@@ -1,12 +1,9 @@
 package chapter_04;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.maxBy;
+import static java.util.stream.Collectors.*;
 
 public class ex5_chapter6 {
 
@@ -35,6 +32,20 @@ public class ex5_chapter6 {
         //maxBy로 최댓값 찾기
         Optional<Dish> mostCalorieDish = specialMenu.stream().collect(maxBy(dishCaloriesCompartor));
         System.out.println(mostCalorieDish);
+
+
+        //위에는 메뉴 리스트의 총 칼로리를 계산하는 코드이다. (요약 연산 예제)
+        //모든 트랜잭션을 돌리고 getCalories를 구한 다음에 이제 그 값을 하나씩 더하는 형태이다.
+        int totalCalories = specialMenu.stream().collect(summingInt(Dish::getCalories));
+        System.out.println(totalCalories);
+
+        //같은 형식으로 여러가지 메서드들을 사용이 가능하다.
+        Double totalCalories_averag = specialMenu.stream().collect(averagingInt(Dish::getCalories));
+        System.out.println(totalCalories_averag);
+
+        //이 함수는 종합적으로 요소수 요리의 칼로리 합계 평균 최대 최소 등등을 계산하는 값이다.
+        IntSummaryStatistics menuStatiestics = specialMenu.stream().collect(summarizingInt(Dish::getCalories));
+        System.out.println("menuStatiestics:" + menuStatiestics);
 
 
 
